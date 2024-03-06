daaddffeff27161479fcd25537f9a33a90c68358
sebasbn29 <sebastianburbano06@gmail.com>
2024-03-06 18:31:12 -0500

    estructura de la clase cuenta

--- Cuenta.ts
+++ Cuenta.ts
@@ -1,3 +1,19 @@
 class Cuenta {
-    
+
+    private numRetiro: number = 0;
+    private numConsignacion: number = 0;
+    private saldo: number
+    private tasaAnual: number
+    private comisionMes: number = 0;
+
+    constructor(numRetiro: number, numConsignacion: number, saldo: number, tasaAnual: number, comisionMes: number) {
+
+        this.numRetiro = numRetiro
+        this.numConsignacion = numConsignacion
+        this.saldo = saldo
+        this.tasaAnual = tasaAnual
+        this.comisionMes = comisionMes
+
+    }
 }
+
