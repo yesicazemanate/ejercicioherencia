68a20860f06f110dea90cd246881a1871bf49df3
sebasbn29 <sebastianburbano06@gmail.com>
2024-03-06 18:41:57 -0500

    estructura de la clase cuenta

--- Cuenta.ts
+++ Cuenta.ts
@@ -1,10 +1,10 @@
 class Cuenta {
 
-    private numRetiro: number = 0;
-    private numConsignacion: number = 0;
-    private saldo: number
-    private tasaAnual: number
-    private comisionMes: number = 0;
+    protected numRetiro: number = 0;
+    protected numConsignacion: number = 0;
+    protected saldo: number
+    protected tasaAnual: number
+    protected comisionMes: number = 0;
 
     constructor(numRetiro: number, numConsignacion: number, saldo: number, tasaAnual: number, comisionMes: number) {
 
@@ -14,6 +14,31 @@ class Cuenta {
         this.tasaAnual = tasaAnual
         this.comisionMes = comisionMes
 
+    }
+
+    get NumRetiro(): number { 
+        return this.numRetiro
+    }
+    set NumRetiro(numRetiro: number) { 
+        this.numRetiro = numRetiro
+    }
+    get NumConsignacion(): number {
+        return this.numConsignacion
+    }
+    set NumConsignacion(numConsignacion: number) {
+        this.numConsignacion = numConsignacion
+    }
+    get TasaAnual(): number { 
+        return this.tasaAnual
+    }
+    set TasaAnual(tasaAnual: number) {
+        this.tasaAnual = tasaAnual
+    }
+    get ComisionMes(): number {
+        return this.comisionMes
+    }
+    set ComisionMes(comisionMes: number) {
+        this.comisionMes = comisionMes
     }
 }
 
