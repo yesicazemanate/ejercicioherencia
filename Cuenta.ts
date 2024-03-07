8da5e9f90b32f0933e3eeb2a512a743e5aeef8e5
ricardo123nac <esteban1525marti@gmail.com>
2024-03-06 19:00:55 -0500

    maryu

--- Cuenta.ts
+++ Cuenta.ts
@@ -1,10 +1,10 @@
-class Cuenta {
+export default class Cuenta {
 
-    private numRetiro: number = 0;
-    private numConsignacion: number = 0;
-    private saldo: number
-    private tasaAnual: number
-    private comisionMes: number = 0;
+    protected numRetiro: number = 0;
+    protected numConsignacion: number = 0;
+    protected saldo: number
+    protected tasaAnual: number
+    protected comisionMes: number = 0;
 
     constructor(numRetiro: number, numConsignacion: number, saldo: number, tasaAnual: number, comisionMes: number) {
 
