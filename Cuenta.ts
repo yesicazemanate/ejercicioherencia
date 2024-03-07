3a93de97151dc9e665d64a0f6f0a12c088bbd951
Luuix02 <Lufvi02@gmail.com>
2024-03-07 14:13:16 -0500

    camb

--- Cuenta.ts
+++ Cuenta.ts
@@ -58,14 +58,8 @@
         if(retiro<= this.saldo){
         let retiroTotal = this.saldo-retiro
         return `el saldo que se retiro es ${retiro}} el saldo total es ${retiroTotal}`
-        } else {
-<<<<<<< HEAD
-            return "No se puede retirar el dinero por saldo insuficiente"  
-=======
-        return "El saldo es insuficiente"
->>>>>>> cuentaAhorros
-        }
-
+        }   
+    
     }
     calcularInteresMensual(){
         let interes= this.saldo * this.tasaAnual
