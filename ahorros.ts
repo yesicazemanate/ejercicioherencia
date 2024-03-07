14e4e3c2795a57c60ce34b527731a35361b73588
Luuix02 <Lufvi02@gmail.com>
2024-03-07 14:34:17 -0500

    cambiosNew

--- ahorros.ts
+++ ahorros.ts
@@ -1,19 +1,19 @@
 import Cuenta from "./Cuenta";
 
-class Ahorros extends Cuenta{
+class Ahorros extends Cuenta {
     private estado: boolean;
 
-    constructor(numRetiro: number, numConsignacion: number, saldo: number,tasaAnual:  number, comisionMes: number ) {
+    constructor(numRetiro: number, numConsignacion: number, saldo: number, tasaAnual: number, comisionMes: number) {
         super(numRetiro, numConsignacion, saldo, tasaAnual, comisionMes);
         this.estado = saldo >= 10000;
-        
+
 
     }
 
 
     //Metodos
 
-    getEstado(): boolean { 
+    getEstado(): boolean {
         return this.estado;
 
     }
@@ -21,7 +21,7 @@ class Ahorros extends Cuenta{
 
     consignar(consignacion: number) {
         if (this.estado) {
-             super.consignar(consignacion);
+            super.consignar(consignacion);
         } else {
             console.log("No se puede consignar el  dinero. La cuenta está inactiva.");
         }
@@ -34,18 +34,18 @@ class Ahorros extends Cuenta{
             console.log("No se puede retirar el  dinero. La cuenta está inactiva.");
         }
     }
-    
-    extractoMensual(): void{
+
+    extractoMensual() {
         const saldo = this.getSaldo();
         const estado = saldo >= 10000 ? "Cuenta activa" : "Cuenta inactiva";
         const retirosTotal = this.getNumRetiro();
         let comisionAdicional = 0;
-        if(retirosTotal > 4 ){
+        if (retirosTotal > 4) {
             comisionAdicional = (retirosTotal - 4) * 1000;
         }
 
         const comisionTotal = this.getComisionMes() + comisionAdicional;
-    
+
         const saldoFinal = saldo - comisionTotal;
 
         // Imprimir el extracto mensual
@@ -56,4 +56,10 @@ class Ahorros extends Cuenta{
         // console.log("Saldo final después de deducir comisiones:", saldoFinal);
     }
 }
- 
+
+// imprimir(){
+//     console.log("Saldo actual: " + this.saldo);
+//     console.log("Comisión mensual: " + this.comisionMensual);
+//     console.log("Número de transacciones realizadas: " + this.numTransacciones);
+
+// }
